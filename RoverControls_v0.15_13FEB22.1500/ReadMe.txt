097145320654f5b8e1f804b6f1c0798fe85c6dea
Denis_M <92692432+deemano@users.noreply.github.com>
2022-02-14 21:15:42 +0000

    Add files via upload
    This Console App is the dashboard that performs all controls and data gathering from the Formula Allcode Rover.
    Just a few things are to be implemented - this is NOT the final version. A few things are still to be implemented. For more details, read the ReadMe.txt file.

---
+++ RoverControls_v0.15_13FEB22.1500/ReadMe.txt
@@ -0,0 +1,49 @@
+========================================================================
+    CONSOLE APPLICATION : Rover_Control_commands Project Overview
+========================================================================
+
+Things to be done:
+1. Integrate IR sensor readings and map those into real distance
+2. Option to edit rove LCD screen
+3. Option to increese speed (from 30 default by a 10th until 100 max)
+4. option to turn on Lights (8 LED front lights)
+5. Maybe the option to memorize turns and to use those to turn back
+6. Apply graphics to all app (preferable ImGUI)
+
+
+AppWizard has created this Rover_Control_commands application for you.
+
+This file contains a summary of what you will find in each of the files that
+make up your Rover_Control_commands application.
+
+
+Rover_Control_commands.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard.
+    It contains information about the version of Visual C++ that generated the file, and
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+Rover_Control_commands.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+Rover_Control_commands.cpp
+    This is the main application source file.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named Rover_Control_commands.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
+Other notes:
+
+AppWizard uses "TODO:" comments to indicate parts of the source code you
+should add to or customize.
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ RoverControls_v0.15_13FEB22.1500/RoverControls.cpp
@@ -0,0 +1,229 @@
+/*  // ==================================================================== \\
+   ||                                                                        ||
+   ||  Console app to control Formula Allcode robot-rover, model RB4420      ||
+   ||  This is the LIGHT version, without the rover pre-defined commands     ||
+   ||  Date: 5th Feb 2022. © Denis Manolescu, Liverpool Hope University      ||
+   ||                                                                        ||
+    \\ ==================================================================== //
+ */
+#include "stdafx.h"
+#include "FA_API.h"
+#include <iostream>
+#include <windows.h>
+#include <conio.h>
+
+
+using namespace std;
+// define symbolic constant names & hexadecimal values to identify keypresses
+
+#define VK_KEY_W	0x57                //('W')	W  forward
+#define VK_KEY_A	0x41                //('A')	A  left
+#define VK_KEY_D	0x44                //('D')	D  right
+#define VK_KEY_S	0x53                //('S')	S  backward
+#define VK_KEY_Q	0x51                //('Q')	Q  cancel reverse keys
+
+class DeviceOne {
+public:
+    // Member to use later to link to GUI
+
+    void forward() {};
+    void backward() {};
+    void left() {};
+    void right() {};
+    void reverse() {};
+    void cancel_reverse() {};
+    char comPort_dvc1 = 4;
+};
+
+class MovementKeys {
+    // member variables:
+private:
+    // keeping reference to deviceOne instead of copy; 
+    // main() has to make sure deviceOne object out-lives MovementKeys object
+    DeviceOne& deviceOne;
+    int key_forward, key_backward, key_left, key_right, key_reverse, cancel_reverse;
+
+public:
+    char iPort = deviceOne.comPort_dvc1;
+    int reset = 0;
+    int reverse = 0;
+    // constructor, which only sets deviceOne and keeps the keys name at their Default
+    // deviceOne must be initialized before constructor function body
+    // as references have no default value
+
+    MovementKeys(DeviceOne& gr) : deviceOne(gr)
+    { // Default list:
+
+        key_forward     = VK_UP;
+        key_backward    = VK_DOWN;
+        key_left        = VK_LEFT;
+        key_right       = VK_RIGHT;
+        key_reverse     = VK_RSHIFT;
+        cancel_reverse  = VK_BACK;
+        int reverse = 0;
+        /*string name;
+        int comPort;*/
+    }
+
+    // loop to define rules of the keys pressed commands
+    void definedCommand()
+    {
+        //FA_ComOpen(iPort);
+        while (true && reset < 2)
+        {
+            // Forwards driving
+            if (GetAsyncKeyState(key_forward) < 0)
+            {
+                cout << ("forward\n");
+                FA_SetMotors(iPort, 30, 30);
+                if (GetAsyncKeyState(key_forward) == 0)
+                {
+                    cout << ("Forwards stopped. \n");
+                    FA_SetMotors(iPort, 0, 0);
+                }
+            }
+            // Backwards driving
+            if (GetAsyncKeyState(key_backward) < 0)
+            {
+                cout << ("backwards\n");
+                FA_SetMotors(iPort, -30, -30);
+                if (GetAsyncKeyState(key_backward) == 0)
+                {
+                    cout << ("Backwards stopped. \n");
+                    FA_SetMotors(iPort, 0, 0);
+                }
+            }
+            //====================================================
+            // programming LEFT & RIGHT keys to perfom 2 actions 
+            // Forward-Left or Forward-Right and Backwards-Left or Backwards-Right
+            if (GetAsyncKeyState(key_reverse) < 0) { reverse = 1; }
+            if (reverse == 0) {
+                // Forwards-Left driving
+                if (GetAsyncKeyState(key_left) < 0)
+                {
+                    cout << ("Forwards left.\n");
+                    FA_SetMotors(iPort, 5, 30);
+                    if (GetAsyncKeyState(key_left) == 0)
+                    {
+                        cout << ("Forwards left stopped. \n");
+                        FA_SetMotors(iPort, 0, 0);
+                    }
+                }
+                // Forwards-Right driving
+                if (GetAsyncKeyState(key_right) < 0)
+                {
+                    cout << ("Forwards right.\n");
+                    FA_SetMotors(iPort, 30, 5);
+                    if (GetAsyncKeyState(key_right) == 0)
+                    {
+                        cout << ("Forwards right stopped. \n");
+                        FA_SetMotors(iPort, 0, 0);
+                    }
+                }
+            }
+            if (reverse == 1) {
+                // Backwards-Left driving
+                if (GetAsyncKeyState(key_left) < 0)
+                {
+                    cout << ("back left\n");
+                    FA_SetMotors(iPort, -5, -30);
+                    if (GetAsyncKeyState(key_left) == 0)
+                    {
+                        cout << ("Back left stopped. \n");
+                        FA_SetMotors(iPort, 0, 0);
+                    }
+                }
+                if (GetAsyncKeyState(key_right) < 0)
+                {
+                    cout << ("backwards right\n");
+                    // Backwards-Right driving
+                    if (GetAsyncKeyState(key_right) < 0)
+                    {
+                        cout << ("Back right\n");
+                        FA_SetMotors(iPort, -30, -5);
+                        if (GetAsyncKeyState(key_right) == 0)
+                        {
+                            cout << ("Back right stopped. \n");
+                            FA_SetMotors(iPort, 0, 0);
+                        }
+                    }
+                }
+            }
+            if (GetAsyncKeyState(cancel_reverse) < 0) { reverse = 0; }
+            // ====================================================
+            // OPTIONAL change keys from within loop with ths code:
+            // key_forward = VK_KEY_W; key_backward = VK_KEY_S; key_left = VK_KEY_A; key_right = VK_KEY_D;
+            if (GetAsyncKeyState(VK_LCONTROL) < 0 && reset == 0) {
+                cout << ("Main control changed to W - A - S - D letters.\n");
+                reset++;
+                break;
+            }
+            if (GetAsyncKeyState(VK_RCONTROL) < 0 && reset == 1) {
+                cout << ("Main control reset to default - Arrow Keys.\n");
+                reset--;
+                break;
+            }
+            // define Honk
+            if (GetAsyncKeyState(VK_TAB) < 0) {
+                if (GetAsyncKeyState(VK_TAB) < 0) {
+                    FA_PlayNote(iPort, 50, 280);
+                    FA_PlayNote(iPort, 50, 286);
+                    Sleep(3000);
+                }
+            }
+            // option to exit program
+            if (GetAsyncKeyState(VK_SPACE) < 0)
+            {
+                FA_ComClose(iPort);
+                exit(0);
+            }
+        }
+    }
+    // function overloading
+    void sounds(int start) {
+            FA_PlayNote(iPort, 500, start); // make start 100
+            FA_PlayNote(iPort, 600, start);
+    }
+    void sounds(int start, int honk) {
+        FA_PlayNote(iPort, start, 100); // start = 500
+        FA_PlayNote(iPort, honk, 100); // honk = 600
+    }
+
+    // function to change the key control values stored in the member variables
+    // can be called by constructor or externally
+    void changeKeys(int change_key_forward, int change_key_backward, int change_key_left, int change_key_right, int change_key_reverse, int change_key_cancel_reverse)
+    {
+        key_forward = change_key_forward;
+        key_backward = change_key_backward;
+        key_left = change_key_left;
+        key_right = change_key_right;
+        key_reverse = change_key_reverse;
+        cancel_reverse = change_key_cancel_reverse;
+    }
+};
+
+int main()
+{
+    DeviceOne deviceOne; // making a class object
+    FA_ComOpen(deviceOne.comPort_dvc1); // open device COM port
+
+    // 1.       making a class object referenced to previous class object
+    // 2/3/4.   calling its member methods
+    while (true) {
+        MovementKeys control(deviceOne);
+        control.sounds(100); 
+        control.definedCommand();
+        control.changeKeys(VK_KEY_W, VK_KEY_S, VK_KEY_A, VK_KEY_D, VK_LSHIFT, VK_KEY_Q);
+        control.sounds(300, 400);
+        control.definedCommand();
+    }
+    FA_ComClose(deviceOne.comPort_dvc1); // closing device COM port
+}
+
+// TO implement:
+//
+//	//Print a string to the LCD
+//	FA_LCDClear(PortNumber);
+//	FA_LCDPrintString(PortNumber, 19, 8, (unsigned char *) "Rover_Control_commands");
+//	FA_LCDPrintString(PortNumber, 31, 16, (unsigned char *) "C++ Example");
+//
