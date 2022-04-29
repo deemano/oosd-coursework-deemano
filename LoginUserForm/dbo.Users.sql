85f85d371e92593997e4775a91c52f55303e1082
Hammada Ballali Ebbi <hammadaballali@gmail.com>
2022-04-29 12:09:30 +0100

    Last version of the login form

---
+++ LoginUserForm/dbo.Users.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [name]     VARCHAR (100) NOT NULL,
+    [password] VARCHAR (MAX) NOT NULL,
+    [secretQ] VARCHAR(MAX) NOT NULL, 
+    [secretA] VARCHAR(MAX) NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
