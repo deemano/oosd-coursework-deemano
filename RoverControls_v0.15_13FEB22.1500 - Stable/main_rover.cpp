0f3fc87deddfa3e8d3538dcc675a75deb4456916
Denis_Manolescu <92692432+deemano@users.noreply.github.com>
2022-05-08 22:26:12 +0100

    Update main_rover.cpp

--- RoverControls_v0.15_13FEB22.1500 - Stable/main_rover.cpp
+++ RoverControls_v0.15_13FEB22.1500 - Stable/main_rover.cpp
@@ -1,14 +1,3 @@
-﻿
-/*  // ==================================================================== \\
-   ||                                                                        ||
-   ||  Console app to control Formula Allcode robot-rover, model RB4420      ||
-   ||  This is the console version, without the graphics,                    ||
-   ||  Date: 5th Feb - 5th May 2022.                                         ||
-   ||  © Denis Manolescu, Liverpool Hope University                          ||
-   ||                                                                        ||
-    \\ ==================================================================== //
- */
-
 #include "stdafx.h"
 #include "FA_API.h"
 #include <iostream>
@@ -20,38 +9,32 @@ using namespace std;
 
 int main()
 {
-    // Constructors declaration
-    DeviceOne deviceOne{}; // making a class object
+    // Class objects:
+    DeviceOne deviceOne;
     Checker checker;
     Data data;
-    
-    // COM port autoscan & autodetect device
-    checker.autoScanPort();  // uncomment this when release
 
-    // Pull out COM port number from checker class
-    int iPort = checker.migrateCom();
+    // COM port autoscan & autodetect device method
+    deviceOne.autoSet_comPort(); 
 
-    cout << "You device is connected to COM port " << iPort << ".\n";
     // Write data on device LCD
     data.writeOnLCD();
 
-    // read distances to objects
-    data.irRadar();
-
-    // Main LOOP
+    // Main rover control LOOP
     while (true) {
         // making class object referenced to previous class object
         MovementKeys control(deviceOne);
 
-        /* Calling member methods :
-           Basic movement commands function */
+        /* Calling member methods =========
+        Main basic movement commands function */
         control.definedCommand();
 
-        // Overriding move commands keys & call control function again
+        // Overriding default move-command keys if user prefer switching
         control.changeKeys(VK_KEY_W, VK_KEY_S, VK_KEY_A, VK_KEY_D, VK_LSHIFT);
+
+        // Re-enter main control method
         control.definedCommand();
     }
-    FA_ComClose(iPort); // closing device COM port
 }
 
 // Implemented:
@@ -65,10 +48,11 @@ int main()
     7. Polymorphism (run-time polymorphism via func overriding) ✓
     8. Functions                                                ✓
     9. Pointers & references                                    ✓
-    10. Turn ON / OFF LED lights            ✓
-*/
+    10. Turn ON / OFF LED lights                                ✓
 
-/*
+
+Others:
+
 1. Objects and Classes                      ✓
 2. Inheritance                              ✓
 3. Polymorphism                             ✓
